87b011948343c16d4615dfd13d4ece3c157d7bc7
Christian Kohout <christian.kohout@gmx.at>
2025-06-17 13:05:12 +0000

    Completed Challenge 4

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -12,8 +12,30 @@ public class Ticket {
 
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public void setPrice(double newPrice) {
+    price = newPrice;
+  }
+
+  public void setIsReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
 
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+  public String getDestination() {
+    return destination;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public boolean getIsReturn() {
+    return isReturn;
+  }
 
 }
