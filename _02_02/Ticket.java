063df4e1d1ba1c7878260d24e577ac9dfa61c81e
Christian Kohout <christian.kohout@gmx.at>
2025-06-17 12:54:50 +0000

    Completed Challenge 3

---
+++ _02_02/Ticket.java
@@ -0,0 +1,14 @@
+package _02_02;
+
+public class Ticket {
+
+  private String destination;
+  private double price;
+  private boolean isReturn;
+
+  public Ticket() {
+    destination = "Vienna";
+    price = 6.50;
+    isReturn = false;
+  }
+}
