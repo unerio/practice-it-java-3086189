82a6a30dc020aa2b667ea54cb6930c44cd896922
Christian Kohout <christian.kohout@gmx.at>
2025-06-18 14:09:21 +0000

    Completed Challenge 7

--- _03_04/ForLoops.java
+++ _03_04/ForLoops.java
@@ -4,11 +4,19 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
+    for (int ind = 0; ind < 5; ind++) {
+      System.out.println("I love for loops");
+    }
 
     // Write a for loop that prints out the numbers 1 to 10
+    for (int ind = 1; ind <= 10; ind++) {
+      System.out.println(ind);
+    }
 
     // Write a for loop that prints out the numbers 10 to 1
-    
+    for (int ind = 10; ind > 0; ind--) {
+      System.out.println(ind);
+    }
 
   }
 
